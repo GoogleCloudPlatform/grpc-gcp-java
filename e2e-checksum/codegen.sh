a0809821c3833b70294eb35d6f22b613a17aefed
Weiran Fang <weiranf@google.com>
2019-06-26 16:37:03 -0700

    ignore generated code

--- e2e-checksum/codegen.sh
+++ e2e-checksum/codegen.sh
@@ -5,6 +5,8 @@ GOOGLEAPIS_DIR=../third_party/googleapis
 GRPC_JAVA_PLUGIN=./protoc-gen-grpc-java
 GENERATED_PROTO_DIR=./src/main/java
 
+rm -rf ${GENERATED_PROTO_DIR}/com/google
+
 for p in $(find ${GOOGLEAPIS_DIR}/google/datastore ${GOOGLEAPIS_DIR}/google/api ${GOOGLEAPIS_DIR}/google/type ${GOOGLEAPIS_DIR}/google/longrunning -type f -name *.proto); do
   protoc \
     --plugin=protoc-gen-grpc-java=${GRPC_JAVA_PLUGIN} \
