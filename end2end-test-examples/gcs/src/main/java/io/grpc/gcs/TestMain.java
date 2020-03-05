7f5e7f56563f1f38d572dc9684bb771ee61871be
Weiran Fang <weiranf@google.com>
2020-03-05 15:37:45 -0800

    Output results to json file

--- end2end-test-examples/gcs/src/main/java/io/grpc/gcs/TestMain.java
+++ end2end-test-examples/gcs/src/main/java/io/grpc/gcs/TestMain.java
@@ -5,36 +5,59 @@ import static io.grpc.gcs.Args.CLIENT_GCSIO_HTTP;
 import static io.grpc.gcs.Args.CLIENT_GRPC;
 import static io.grpc.gcs.Args.CLIENT_YOSHI;
 
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.Security;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.logging.Logger;
 import org.conscrypt.Conscrypt;
-import java.security.Security;
 
 public class TestMain {
   private static final Logger logger = Logger.getLogger(TestMain.class.getName());
 
-  private static void printResult(ArrayList<Long> results) {
-    if (results.size() == 0) return;
-
-    Collections.sort(results);
-    int n = results.size();
-    System.out.println(
-        String.format( "============ Test Results: \n"
-                + "\t\tMin\tp5\tp10\tp25\tp50\tp75\tp90\tp99\tMax\n"
-                + "  Time(ms)\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d",
-            results.get(0),
-            results.get((int) (n * 0.05)),
-            results.get((int) (n * 0.1)),
-            results.get((int) (n * 0.25)),
-            results.get((int) (n * 0.50)),
-            results.get((int) (n * 0.75)),
-            results.get((int) (n * 0.90)),
-            results.get((int) (n * 0.99)),
-            results.get(n - 1)));
+  private static class BenchmarkResult {
+    public Long min, p50, p90, p99, p999;
+    public double qps;
   }
 
-  public static void main(String[] args) throws Exception{
+  private static void printResult(FileWriter writer, ArrayList<Long> results)
+      throws IOException {
+    if (results.size() == 0) return;
+    Collections.sort(results);
+    int n = results.size();
+    double totalSeconds = 0;
+    for (Long ms : results) {
+      totalSeconds += ms / 1000.0;
+    }
+
+    Gson gson = new Gson();
+    BenchmarkResult benchmarkResult = new BenchmarkResult();
+    benchmarkResult.min = results.get(0);
+    benchmarkResult.p50 = results.get((int) (n * 0.05));
+    benchmarkResult.p90 = results.get((int) (n * 0.90));
+    benchmarkResult.p99 = results.get((int) (n * 0.99));
+    benchmarkResult.p999 = results.get((int) (n * 0.999));
+    benchmarkResult.qps = n / totalSeconds;
+    gson.toJson(benchmarkResult, writer);
+    writer.close();
+    System.out.println(String.format(
+        "============ Test Results: \n"
+            + "\t\tMin\tp5\tp10\tp25\tp50\tp75\tp90\tp99\tMax\n"
+            + "  Time(ms)\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+        results.get(0),
+        results.get((int) (n * 0.05)),
+        results.get((int) (n * 0.1)),
+        results.get((int) (n * 0.25)),
+        results.get((int) (n * 0.50)),
+        results.get((int) (n * 0.75)),
+        results.get((int) (n * 0.90)),
+        results.get((int) (n * 0.99)),
+        results.get(n - 1)));
+  }
+
+  public static void main(String[] args) throws Exception {
     Args a = new Args(args);
     if (a.conscrypt) {
       Security.insertProviderAt(Conscrypt.newProvider(), 1);
@@ -64,6 +87,7 @@ public class TestMain {
       default:
         logger.warning("Please provide --client");
     }
-    printResult(results);
+    FileWriter writer = new FileWriter(a.latencyFilename + ".json");
+    printResult(writer, results);
   }
 }
