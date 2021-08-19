8d64709be4913eae3f5cd839fc4111a4428db600
Esun Kim <veblush@google.com>
2021-08-19 10:00:16 -0700

    Sync with hadoop-connector

--- end2end-test-examples/gcs/src/main/java/io/grpc/gcs/ZeroCopyMessageMarshaller.java
+++ end2end-test-examples/gcs/src/main/java/io/grpc/gcs/ZeroCopyMessageMarshaller.java
@@ -10,11 +10,10 @@ import io.grpc.Detachable;
 import io.grpc.HasByteBuffer;
 import io.grpc.KnownLength;
 import io.grpc.MethodDescriptor.PrototypeMarshaller;
-import io.grpc.protobuf.lite.ProtoLiteUtils;
 import io.grpc.Status;
-import io.grpc.MethodDescriptor;
-import java.io.InputStream;
+import io.grpc.protobuf.lite.ProtoLiteUtils;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -22,76 +21,80 @@ import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
 
-// Custom gRPC marshaller to use zero memory copy feature of gRPC when deserializing messages.
-// This achieves zero-copy by deserializing proto messages pointing to the buffers in the input
-// stream to avoid memory copy so stream should live as long as the message can be referenced.
-// Hence, it exposes the input stream to applications (through popStream) and applications are
-// responsible to close it when it's no longer needed. Otherwise, it'd cause memory leak.
+/**
+ * Custom gRPC marshaller to use zero memory copy feature of gRPC when deserializing messages. This
+ * achieves zero-copy by deserializing proto messages pointing to the buffers in the input stream to
+ * avoid memory copy so stream should live as long as the message can be referenced. Hence, it
+ * exposes the input stream to applications (through popStream) and applications are responsible to
+ * close it when it's no longer needed. Otherwise, it'd cause memory leak.
+ */
 class ZeroCopyMessageMarshaller<T extends MessageLite> implements PrototypeMarshaller<T> {
-  private Map<T, InputStream> unclosedStreams = Collections.synchronizedMap(new IdentityHashMap<>());
+  private Map<T, InputStream> unclosedStreams =
+      Collections.synchronizedMap(new IdentityHashMap<>());
   private final Parser<T> parser;
-  private final PrototypeMarshaller<T> baseMarshaller;
+  private final PrototypeMarshaller<T> marshaller;
 
   ZeroCopyMessageMarshaller(T defaultInstance) {
     parser = (Parser<T>) defaultInstance.getParserForType();
-    baseMarshaller = (PrototypeMarshaller<T>) ProtoLiteUtils.marshaller(defaultInstance);
+    marshaller = (PrototypeMarshaller<T>) ProtoLiteUtils.marshaller(defaultInstance);
   }
 
   @Override
   public Class<T> getMessageClass() {
-    return baseMarshaller.getMessageClass();
+    return marshaller.getMessageClass();
   }
 
   @Override
   public T getMessagePrototype() {
-    return baseMarshaller.getMessagePrototype();
+    return marshaller.getMessagePrototype();
   }
 
   @Override
   public InputStream stream(T value) {
-    return baseMarshaller.stream(value);
+    return marshaller.stream(value);
   }
 
   @Override
   public T parse(InputStream stream) {
-    CodedInputStream cis = null;
     try {
-      if (stream instanceof KnownLength) {
+      if (stream instanceof KnownLength
+          && stream instanceof Detachable
+          && stream instanceof HasByteBuffer
+          && ((HasByteBuffer) stream).byteBufferSupported()) {
         int size = stream.available();
-        if (stream instanceof Detachable && ((HasByteBuffer) stream).byteBufferSupported()) {
-          // Stream is now detached here and should be closed later.
-          stream = ((Detachable) stream).detach();
-          // This mark call is to keep buffer while traversing buffers using skip.
-          stream.mark(size);
-          List<ByteString> byteStrings = new ArrayList<>();
-          while (stream.available() != 0) {
-            ByteBuffer buffer = ((HasByteBuffer) stream).getByteBuffer();
-            byteStrings.add(UnsafeByteOperations.unsafeWrap(buffer));
-            stream.skip(buffer.remaining());
-          }
-          stream.reset();
-          cis = ByteString.copyFrom(byteStrings).newCodedInput();
-          cis.enableAliasing(true);
-          cis.setSizeLimit(Integer.MAX_VALUE);
+        // Stream is now detached here and should be closed later.
+        stream = ((Detachable) stream).detach();
+        // This mark call is to keep buffer while traversing buffers using skip.
+        stream.mark(size);
+        List<ByteString> byteStrings = new ArrayList<>();
+        while (stream.available() != 0) {
+          ByteBuffer buffer = ((HasByteBuffer) stream).getByteBuffer();
+          byteStrings.add(UnsafeByteOperations.unsafeWrap(buffer));
+          stream.skip(buffer.remaining());
         }
+        stream.reset();
+        CodedInputStream codedInputStream = ByteString.copyFrom(byteStrings).newCodedInput();
+        codedInputStream.enableAliasing(true);
+        codedInputStream.setSizeLimit(Integer.MAX_VALUE);
+        // fast path (no memory copy)
+        T message;
+        try {
+          message = parseFrom(codedInputStream);
+        } catch (InvalidProtocolBufferException ipbe) {
+          stream.close();
+          throw Status.INTERNAL
+              .withDescription("Invalid protobuf byte sequence")
+              .withCause(ipbe)
+              .asRuntimeException();
+        }
+        unclosedStreams.put(message, stream);
+        return message;
       }
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-    if (cis != null) {
-      // fast path (no memory copy)
-      T message;
-      try {
-        message = parseFrom(cis);
-      } catch (InvalidProtocolBufferException ipbe) {
-        throw Status.INTERNAL.withDescription("Invalid protobuf byte sequence").withCause(ipbe).asRuntimeException();
-      }
-      unclosedStreams.put(message, stream);
-      return message;
-    } else {
-      // slow path
-      return baseMarshaller.parse(stream);
-    }
+    // slow path
+    return marshaller.parse(stream);
   }
 
   private T parseFrom(CodedInputStream stream) throws InvalidProtocolBufferException {
