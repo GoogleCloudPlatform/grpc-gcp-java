bde373a84838f11e2c2f5419e10d42c33d905709
Feng Li <fengli@google.com>
2024-11-04 14:19:38 -0800

    Enable xDS logs and upgrade grpc version to 1.68.1. (#186)

--- continuous_load_testing/src/main/java/com/google/net/grpc/testing/directpath/continuous_load_testing/Client.java
+++ continuous_load_testing/src/main/java/com/google/net/grpc/testing/directpath/continuous_load_testing/Client.java
@@ -6,11 +6,23 @@ import io.grpc.testing.integration.EmptyProtos.Empty;
 import io.grpc.testing.integration.TestServiceGrpc;
 import io.grpc.testing.integration.TestServiceGrpc.TestServiceBlockingStub;
 import io.grpc.testing.integration.TestServiceGrpc.TestServiceStub;
+import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class Client {
+  private static Logger logger = Logger.getLogger(Client.class.getName());
   private static String BACKEND = "google-c2p:///directpathgrpctesting-pa.googleapis.com";
 
+
   public static void main(String[] args) {
+    try {
+      LogManager.getLogManager()
+          .readConfiguration(Client.class.getResourceAsStream("/logging.properties"));
+    } catch (IOException e) {
+      logger.info(e.toString());
+    }
+
     ManagedChannelBuilder builder = ManagedChannelBuilder.forTarget(BACKEND);
     TestServiceBlockingStub stub = TestServiceGrpc.newBlockingStub(builder.build());
     stub.emptyCall(Empty.getDefaultInstance());
